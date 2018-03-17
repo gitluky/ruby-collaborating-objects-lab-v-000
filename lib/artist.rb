74141972de5c00d9250b27a360f65866b57bda35
Jonathan Luk <jluk87@gmail.com>
2018-03-17 16:55:40 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,4 +2,27 @@ require 'pry'
 
 class Artist
 
+  @@all = []
+  attr_accessor :name, :songs
+
+  def initialize(name)
+    @name = name
+    @songs = []
+
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.find_or_create_by_name(artist)
+    if all.include?(artist)
+      artist.name
+    else
+      new_artist = Artist.new(name)
+      all << new_artist
+    end
+
+  end
+
 end
