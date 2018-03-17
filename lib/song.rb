82183ca434f40e730d183c4c81fdcd2a917897c2
Jonathan Luk <jluk87@gmail.com>
2018-03-17 18:13:31 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -8,8 +8,7 @@ class Song
     @name = name
   end
 
-  def self.new_by_filename(name)
-    new_song = self.new(name)
-
+  def self.new_by_filename(filename)
+    song_name = Song.new(filename.split(/\s\-\s/).gsub(".mp3", "").filename[1])
   end
 end
