3fd55e5e8d94477d1a437bc2b115194d556a5e48
Jonathan Luk <jluk87@gmail.com>
2018-03-17 18:02:38 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,5 +2,14 @@ require 'pry'
 
 class Song
 
+  attr_accessor :name, :artist
 
+  def initialize(name)
+    @name = name
+  end
+
+  def self.new_by_filename(name)
+    new_song = self.new(name)
+
+  end
 end
