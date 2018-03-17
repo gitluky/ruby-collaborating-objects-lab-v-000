4813b18e95f198c438568bc2214baeb7772526da
Jonathan Luk <jluk87@gmail.com>
2018-03-17 17:46:04 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -4,10 +4,18 @@ require 'pry'
 
 class MP3Importer
 
-  attr_accessor :path
+  attr_accessor :path,
 
   def initialize(path)
     @path = path
   end
 
+  def files
+    @files = Dir.glob("#{path}/*.mp3")
+  end
+
+
 end
+
+new_import = MP3Importer.new(../db/mp3s)
+puts new_import.files
